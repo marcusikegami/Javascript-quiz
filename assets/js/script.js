6fe1fc5b9373df37449fc464344c899b43cd86ab
Marcus Ikegami <hikegami.6@gmail.com>
2021-10-03 21:13:35 -0600

    added question element with reponse elements. on click the responses carry out a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 const startButton = document.getElementById('start-button');
 const quizBox = document.querySelector('quiz-wrapper');
-const startScreen = document.querySelector('start-screen');
+const startScreen = document.getElementById('start-screen');
+const responseButton = document.querySelector('.response');
+
+
+
+
 
 startButton.addEventListener('click', startQuiz);
+responseButton.addEventListener('click', questionResponseStorage)
 
+quizQuestions = [
+    
+];
+// startQuiz will begin by hiding the start-screen and iterating through the quiz questions array every time a question is answered
 function startQuiz() {
-    startScreen.setAttribute('style', '"display", "none"');
+   startScreen.style.display = 'none';
+
+};
+
+function questionResponseStorage() {
+    console.log('ello');
 };
