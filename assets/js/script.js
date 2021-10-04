6e169ab5da4edb45e57ff9d24f069e1c147065f4
Marcus Ikegami <hikegami.6@gmail.com>
2021-10-03 22:04:27 -0600

    incorrect answer now appends the 'inccorect' div to the body

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
 const startButton = document.getElementById('start-button');
 const quizBox = document.querySelector('quiz-wrapper');
 const startScreen = document.getElementById('start-screen');
-const responseButton = document.querySelector('.response');
 
 
+document.querySelectorAll('.response').forEach(item => {
+    item.addEventListener('click', event => {
+        //handles click
 
+        // this will deduct time from the timer if user answers incorrectly
+        if (item.getAttribute('data-correct') === 'false') {
+            console.log('false')
+
+        } else {
+            const INCORRECT = document.querySelector('.body')
+            const incorrectElement = document.createElement('div')
+            incorrectElement.setAttribute('class', 'incorrect')
+            incorrectElement.textContent ='INCORRECT'
+            INCORRECT.appendChild(incorrectElement)
+        }
+        
+})
+
+
+})
 
 
 startButton.addEventListener('click', startQuiz);
-responseButton.addEventListener('click', questionResponseStorage)
+
 
 quizQuestions = [
     
@@ -18,7 +36,9 @@ function startQuiz() {
    startScreen.style.display = 'none';
 
 };
+// this function needs to log the user's answer and determine if it is incorrect/correct, show the next question, and adjust the timer accordingly
 
-function questionResponseStorage() {
-    console.log('ello');
-};
+// function questionResponseStorage() {
+//     console.log(responseButton.getAttribute('id'))
+    
+
