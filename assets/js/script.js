ff3c4f5428ed2f147e1b191b3c0ae57f45605f18
Marcus Ikegami <hikegami.6@gmail.com>
2021-10-10 08:13:28 -0600

    Added a list of questions to be accessed inside array quizQuestions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,42 @@
 const startButton = document.getElementById('start-button');
 const quizBox = document.querySelector('quiz-wrapper');
 const startScreen = document.getElementById('start-screen');
+let questionDiv = $('.question-text');
+// const INCORRECT =
 
+// const currentQuestion
+quizQuestions = [
+    {
+        question: "What is the correct way to write a comment in Javascript?",
+        answers: [
+            {text: '!-- Comment --!', isTrue: false},
+            {text: '// Comment', isTrue: true},
+            {text: '` Comment', isTrue: false}
+        ]
+      }, {
+      question: "How can you link a javascript file to an HTML document?",
+      answers: [
+          {text: '<link rel="script" href="./script.js">', isTrue: false},
+          {text: '<script href="./script.js"></script>', isTrue: false},
+          {text: '<script src="./script.js"</script>', isTrue: true} 
+        ]
+      }, {
+      question: "What is the correct way to change the text inside and HTML element using Javascript?",
+      answers: [
+          {text: 'document.getElementById("demo").innerHTML("Hello World!")', isTrue: false}, 
+          {text: '#demo.innerHTML = "Hello World!"', isTrue: false},
+          {text: 'document.getElementById("demo").innerHTML = "Hello World!"', isTrue: true}
+        ]
+      }, {
+        question: 'How can you declare "demo" as a Javascript variable?',
+        answers: [
+              {text: 'var demo;', isTrue: true},
+              {text: 'variable demo;', isTrue: false},
+              {text: 'v demo;', isTrue: false}
+        ]
+      }
+      
+  ];
 
 document.querySelectorAll('.response').forEach(item => {
     item.addEventListener('click', event => {
@@ -22,18 +57,28 @@ document.querySelectorAll('.response').forEach(item => {
 })
 
 
-})
+});
 
+
+function nextQuestion() {
+    for (i = 0; i < questions.length; i++) {
+        var question = $('<h1>')
+        .addClass('response')
+        .text(questions.question[i]);
+
+        questionDiv.appendChild(question);
+    }
+}
 
 startButton.addEventListener('click', startQuiz);
 
 
-quizQuestions = [
-    
-];
+
 // startQuiz will begin by hiding the start-screen and iterating through the quiz questions array every time a question is answered
 function startQuiz() {
    startScreen.style.display = 'none';
+    questionDiv.removeClass('d-none');
+
 
 };
 // this function needs to log the user's answer and determine if it is incorrect/correct, show the next question, and adjust the timer accordingly
